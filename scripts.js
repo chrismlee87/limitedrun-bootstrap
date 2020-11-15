6f4cf7b8f17856d475de9205cac84f80ccbe019b
CHRIS_LEE <chrismlee87@gmail.com>
2020-11-14 21:53:09 -0800

    master

---
+++ scripts.js
@@ -0,0 +1,39 @@
+/*!
+    * Start Bootstrap - Creative v6.0.4 (https://startbootstrap.com/theme/creative)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+    */
+   (function($) {
+    "use strict"; // Start of use strict
+  
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 72)
+          }, 1000, "easeInOutExpo");
+          return false;
+        }
+      }
+    });
+
+    // Magnific popup calls
+    $('#portfolio').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+      }
+    });
+  
+  })(jQuery); // End of use strict
